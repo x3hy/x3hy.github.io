b9f76f853bf061de040529ad8840608375609a57
x3hy <tkf.x1os@gmail.com>
2026-03-06 16:55:46 +1300

    small update

--- src/date.js
+++ src/date.js
@@ -1,43 +1,34 @@
-// https://stackoverflow.com/a/3177838
 function time_since (date)
 {
+	const fmt = (time, ext) =>
+		{return `${time} ${ext}${(time >= 1) ? "s" : ""}`}
+
 	const old_date = new Date(date);
 	let seconds = Math.floor((new Date() - old_date) / 1000)
 	
 	var interval = seconds / 31536000;
 	if (interval > 1)
-	  {
-		return Math.floor(interval) + " years";
-	  }
+		return fmt(Math.floor(interval), "year");
 	
 	interval = seconds / 2592000;
 	if (interval > 1)
-	  {
-		return Math.floor(interval) + " months";
-	  }
+		return fmt(Math.floor(interval),"month");
 	
 	interval = seconds / 86400;
 	if (interval > 1)
-	  {
-		return Math.floor(interval) + " days";
-	  }
+		return fmt(Math.floor(interval),"day");
 	
 	interval = seconds / 3600;
 	if (interval > 1)
-	  {
-		return Math.floor(interval) + " hours";
-	  }
+		return fmt(Math.floor(interval), "hour");
 	
 	interval = seconds / 60;
 	if (interval > 1)
-	  {
-		return Math.floor(interval) + " minutes";
-	  }
+		return fmt(Math.floor(interval), "minute");
 	
-	return Math.floor(seconds) + " seconds";
+	return fmt(Math.floor(seconds), "second");
 }
 
-console.log (time_since("2026-01-26 00:32:15.773013+00:00"))
 document.querySelectorAll('.date').forEach(element => {
 	element.innerText = time_since(element.innerText)
 })
