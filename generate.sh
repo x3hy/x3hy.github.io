adfe7bc25dc2078e9d8661bea6b7bb167dc61849
x3hy <tkf.x1os@gmail.com>
2026-06-08 10:50:20 +1200

    Auto-push: Mon Jun  8 10:50:20 AM NZST 2026

--- generate.sh
+++ generate.sh
@@ -10,7 +10,7 @@ fi
 set -xe
 
 # Generate projects
-new=$(plate --template="<tr><td><a href=\"<!--\$html_url-->\"><span title="Language"></span> <b><!--\$name--></a></b></td><td><i><!--\$description--></i></td></tr>" -i=source.html -J=data/repo.json -L=!PLATE_PROJECTS -p="data")
+new=$(plate --template="<tr><td><a href=\"<!--\$html_url-->\"><span title="Language"></span> <b><!--\$name--></b></a></td><td><!--\$description--></td></tr>" -i=source.html -J=data/repo.json -L=!PLATE_PROJECTS -p="data")
 
 echo "$new" > index.html
 echo "Loaded projects"
